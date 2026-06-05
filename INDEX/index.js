1c3ee58cfacc997b95e0f235a1f31075682a60a9
luciamacellaro <lucia.macellaro@anima.edu.uy>
2026-06-05 19:40:19 -0300

    rutas

--- INDEX/index.js
+++ INDEX/index.js
@@ -1,3 +1,3 @@
 document.getElementById("btnEntrar").addEventListener("click", () => {
-  window.location.href = "/Final-proyect-firstsem-Frontend/HOME/home.html";
+  window.location.href = "/HOME/home.html";
 });
