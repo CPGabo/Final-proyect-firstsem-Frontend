02472a6002a2619825f4faf754ff929e09514ac3
luciamacellaro <lucia.macellaro@anima.edu.uy>
2026-06-08 20:51:53 -0300

    api-clima emoji

--- HOME/script.js
+++ HOME/script.js
@@ -70,6 +70,15 @@ cargarArticulos();
 
 
 // API - CLIMA
+function emojiClima(code) {
+if (code === 0) return "☀️";
+if (code <= 3) return "⛅";
+if (code <= 48) return "☁️";
+if (code <= 67) return "️";
+if (code <= 77) return "❄️";
+if (code <= 82) return "️";
+return "⚡";
+}
 async function cargarClima() {
 const clima = document.querySelector("#clima");
 try {
@@ -81,9 +90,10 @@ if (!response.ok) throw new Error("Error al obtener el clima");
 const datos = await response.json();
 const temp = datos.current_weather.temperature;
 const viento = datos.current_weather.windspeed;
+const emoji = emojiClima(datos.current_weather.weathercode);
 clima.innerHTML = `
 <p class="mb-0">
-️ Montevideo — <strong>${temp}°C</strong>
+${emoji} Montevideo — <strong>${temp}°C</strong>
 &nbsp;|&nbsp;
 💨 Viento: ${viento} km/h
 </p>
@@ -94,4 +104,5 @@ disponible.</p>`;
 console.error(error);
 }
 }
-cargarClima();
+
+cargarClima()
